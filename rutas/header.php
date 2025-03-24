2ea5fa3c58a1a9fecf10eb7859524d33064f0474
Geovanni-Vera <geovanni.vera23@gmail.com>
2025-03-24 00:28:15 -0600

    second commit

--- rutas/header.php
+++ rutas/header.php
@@ -12,6 +12,7 @@ session_start(); // Inicia la sesión si la necesitas en todas las páginas
     <link rel="stylesheet" href="bootstrap/bootstrap-5.3.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <link rel="stylesheet" href="css/estilos.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/495fbb564e.js" crossorigin="anonymous"></script>
 </head>
 
